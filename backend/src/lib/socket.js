18f67e7869d99090c01280183aa6104ee4d77a4b
HarshalRane123 <harshalr@mitrmedia.com>
2025-01-17 17:59:24 +0530

    Updated socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chatapp-nmit.onrender.com"],
+    origin: [
+      "http://localhost:5173",
+      "https://chatapp-nmit.onrender.com",
+      "http://localhost:5001",
+    ],
     // methods: ["GET", "POST", "PUT"], // Added PUT method
     credentials: true, // Enable credentials if needed
   },
